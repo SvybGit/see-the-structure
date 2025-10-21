1b4a002bc96b98f9efa2694a9e43f4219a24bb4d
Šimon Vybíral <xvybira2@fi.muni.cz>
2025-10-22 00:18:07 +0200

    fix: Improve StackNode and Stack encapsulation, make Stack Iterable, Stack general improvements

--- src/data-structures/stack.ts
+++ src/data-structures/stack.ts
@@ -1,43 +1,75 @@
 class StackNode<T> {
-    public nextNode?: StackNode<T>;
-    public value: T;
+    private nextNode?: StackNode<T>;
+    public readonly value: T;
 
     constructor(value: T, nextNode?: StackNode<T>) {
         this.value = value;
         this.nextNode = nextNode;
     }
+
+    public next() {
+        return this.nextNode;
+    }
+
+    public unlink() {
+        this.nextNode = undefined;
+    }
 }
 
-class Stack<T> {
+export class Stack<T> implements Iterable<T> {
     private topNode?: StackNode<T>;
-    private size: number = 0;
+    private internalSize: number = 0;
 
-    constructor(values?: T[]) {
+    get size() {
+        return this.internalSize;
+    }
+
+    /**
+     * Instantiates a Stack data structure
+     * @param values are going to be pushed into the stack in forward order eg. last element will be popped first
+     */
+    constructor(values?: Iterable<T>) {
         if (values !== undefined) {
-            values.forEach(value => this.push(value))
+            for (const value of values) {
+                this.push(value)
+            }
+        }
+    }
+
+    *[Symbol.iterator](): IterableIterator<T> {
+        let current = this.topNode;
+        while (current) {
+            yield current.value;
+            current = current.next();
         }
     }
 
     public push(value: T): void {
-        this.size++;
-        if (this.topNode === undefined) {
-            this.topNode = new StackNode(value);
-        } else {
-            const oldTopNode = this.topNode;
-            this.topNode = new StackNode(value, oldTopNode);
+        this.internalSize++;
+        this.topNode = new StackNode(value, this.topNode);
+    }
+
+    public pop(): T {
+        if (this.isEmpty()) {
+            throw new RangeError("The stack is empty");
         }
+        const popped = this.topNode!;
+        this.topNode = popped.next();
+        popped.unlink();
+        this.internalSize--;
+        return popped.value;
     }
 
-    public pop(): T | undefined {
-        if (this.topNode !== undefined)
-            this.topNode = this.topNode?.nextNode;
-            const value = this.peek();
-            this.size--;
-            return value
-    }
-
-    public  peek(): T | undefined {
+    public peek(): T | undefined {
         return this.topNode?.value;
     }
 
+    public isEmpty() {
+        return this.topNode === undefined;
+    }
+
+    public clear() {
+        this.topNode = undefined;
+        this.internalSize = 0;
+    }
 }
